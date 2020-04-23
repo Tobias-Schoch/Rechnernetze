7e8617117b574601bed48079ecc7d063a347f038
tobias-schoch <0hausjhdutqeaxekaf73wfu>
2020-04-23 11:06:11 +0200

    Added store times

--- simulation.py
+++ simulation.py
@@ -4,6 +4,14 @@ class Supermarket:
     RespawnA = 200
     RespawnB = 60
 
+    # storetime
+    BakerTime = 10
+    ButcherTime = 30
+    CheeseTime = 60
+    CheckoutTime = 5
+
+    #
     PurchaseTypA = ([10, 30, 45, 60], [10, 10, 5, 20], [10, 5, 3, 30])
     PurchaseTypB = ([30, 30, 20], [5, 20, 20], [2, 3, 3])
+
 
