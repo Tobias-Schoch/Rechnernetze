49bb9265dbbb0ba616c90a09608bf9f710b67c2c
tobias-schoch <0hausjhdutqeaxekaf73wfu>
2020-04-23 11:32:52 +0200

    Added event functions

--- simulation.py
+++ simulation.py
@@ -10,8 +10,17 @@ class Supermarket:
     CheeseTime = 60
     CheckoutTime = 5
 
-    #
+    # customertypes
     PurchaseTypA = ([10, 30, 45, 60], [10, 10, 5, 20], [10, 5, 3, 30])
     PurchaseTypB = ([30, 30, 20], [5, 20, 20], [2, 3, 3])
 
+
+
+def begin():
+
+def move():
+
+def purchase():
+
+def wait():
 
