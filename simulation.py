a33a838d6795db7ffab150319e94e1cb02746c6d
Tobias.Schoch <43953160+Tobias-Schoch@users.noreply.github.com>
2020-04-23 10:45:03 +0200

    First

---
+++ simulation.py
@@ -0,0 +1,9 @@
+class Supermarket:
+    AmountCustomer = 0
+    Time = 0
+    RespawnA = 200
+    RespawnB = 60
+
+    PurchaseTypA = ([10, 30, 45, 60], [10, 10, 5, 20], [10, 5, 3, 30])
+    PurchaseTypB = ([30, 30, 20], [5, 20, 20], [2, 3, 3])
+
