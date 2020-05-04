1539a427bd6fb46748ed1ddc7890fcb115ac196a
tobias-schoch <tobias@schochwebdesign.de>
2020-05-04 14:21:54 +0200

    Added customer thread

--- realzeitsimulation.py
+++ realzeitsimulation.py
@@ -5,13 +5,17 @@ from threading import Event
 from threading import Lock
 import datetime
 
-class Store(Thread):
+"""class Store(Thread):
     def __init__(self, name, dauer):
         self.name = name
         self.dauer = dauer
 
+    def run(self):
+        while not"""
+
 class Customer(Thread):
     def __init__(self, name, todo):
+        Thread.__init__(self)
         self.name = name
         self.todo = list(todo)
         self.actual_todo = self.todo.pop(0)
@@ -19,14 +23,37 @@ class Customer(Thread):
         self.start = datetime.datetime.now()
         self.finish = 0
 
-def generate_customer(sleep, name, todo):
-    # TODO: generater customer
+    def run(self):
+        while not
+
+class Todo:
+    def __init__(self, station, arrival, max_queue, purchase):
+        self.station = station
+        self.arrival = arrival
+        self.max_queue = max_queue
+        self.purchase = purchase
 
 if __name__ == "__main__":
-    baker = Store("Bäcker", 10)
+    """baker = Store("Backer", 10)
     butcher = Store("Metzger", 30)
-    cheese = Store("Käsetheke", 60)
-    checkout = Store("Kasse", 5)
+    cheese = Store("Kasetheke", 60)
+    checkout = Store("Kasse", 5)"""
+
+    customer_a = [Todo(0, 10, 10, 10), Todo(1, 30, 5, 10), Todo(2, 45, 3, 5),
+                   Todo(3, 10, 30, 20)]
+    customer_b = [Todo(1, 30, 2, 5), Todo(3, 30, 3, 20), Todo(0, 20, 3, 20)]
+
+    generate_a = Thread(target=generate_customer, args=(200, "A", customer_a))
+    generate_b = Thread(target=generate_customer, args=(60, "B", customer_b))
+
+    generate_a.start()
+    generate_b.start()
+
+    """baker.start()
+    butcher.start()
+    cheese.start()
+    checkout.start()"""
+
 
     """start_new_thread(baker)
     start_new_thread(butcher)
@@ -37,7 +64,7 @@ def baker1():
     print("baker is sleeping")
     time.sleep(300)
     print("baker woke up")
-t = Thread(target= baker1)
+t = Thread(target = baker1)
 t.start()
 
 
