2f96dec304f34bc1096493693938689e533d54e4
tobias-schoch <tobias@schochwebdesign.de>
2020-05-04 16:51:37 +0200

    Added customer action

--- realzeitsimulation.py
+++ realzeitsimulation.py
@@ -1,5 +1,4 @@
 from threading import Thread
-from _thread import start_new_thread
 import time
 from threading import Event
 from threading import Lock
@@ -14,9 +13,30 @@ class Station(Thread):
         self.stop = Event()
         self.queue = []
         self.working = False
+        self.is_busy = False
+        self.arrEv = Event()
+        self.servEv = Event()
+        self.killEv = Event()
+        self.lock = Lock()
+        self.FACTOR = 10
 
-    # def run(self):
-    # while not
+    def run(self):
+        while not self.killEv.is_set():
+
+            # Wait for a customer
+            # Timeout to detect if the kill flag has been set
+            if self.arrEv.wait(10) is not True:
+                continue
+
+            self.is_busy = True
+            while len(self.queue) != 0:
+                customer = self.queue.pop(0)
+                serving_time = customer.current_task.amount * self.dauer
+                print(f"{self.name} serving customer {customer} for {serving_time} sec.")
+                time.sleep(serving_time // self.FACTOR)
+                customer.servEv.set()
+            self.is_busy = False
+            self.arrEv.clear()
 
 
 class Customer(Thread):
@@ -27,22 +47,29 @@ class Customer(Thread):
         self.actual_todo = self.todo.pop(0)
         self.skipped_todo = []
         # 0 = walking
-        # 1 = waiting
-        # 2 = served
+        # 1 = at station
         self.state = 0
         self.finish = 0
 
     def run(self):
         print(self.name + " is walking.")
+        time.sleep(self.actual_todo.arrival / 20)
         while self.state == 0:
+            print(self.name + " is arrived")
+
+            station = self.actual_todo.station
+            if len(station.queue) > self.actual_todo.queue_length:
+                continue
+            print(self.name + " is waiting at " + station)
+
 
 
 
 class Todo:
-    def __init__(self, station, arrival, max_queue, purchase):
+    def __init__(self, station, arrival, queue_length, purchase):
         self.station = station
         self.arrival = arrival
-        self.max_queue = max_queue
+        self.queue_length = queue_length
         self.purchase = purchase
 
 
@@ -55,7 +82,7 @@ def generate_customer(sleep_time, name, todo):
         customer.append(k)
         customer_lock.release()
         a += 1
-        time.sleep(sleep_time / 30)
+        time.sleep(sleep_time)
 
 
 stop = Event()
@@ -67,25 +94,21 @@ if __name__ == "__main__":
     butcher = Station("Metzger", 30)
     cheese = Station("Kasetheke", 60)
     checkout = Station("Kasse", 5)
+    customer_a = [Todo(0, 10, 10, 10), Todo(1, 30, 5, 10), Todo(2, 45, 3, 5), Todo(3, 10, 30, 20)]
+    customer_b = [Todo(1, 30, 2, 5), Todo(3, 30, 3, 20), Todo(0, 20, 3, 20)]
+    generate_a = Thread(target=generate_customer, args=(200, "A", customer_a))
+    generate_b = Thread(target=generate_customer, args=(60, "B", customer_b))
 
     baker.start()
     butcher.start()
     cheese.start()
     checkout.start()
-
-    customer_a = [Todo(0, 10, 10, 10), Todo(1, 30, 5, 10), Todo(2, 45, 3, 5), Todo(3, 10, 30, 20)]
-    customer_b = [Todo(1, 30, 2, 5), Todo(3, 30, 3, 20), Todo(0, 20, 3, 20)]
-
-    generate_a = Thread(target=generate_customer, args=(200, "A", customer_a))
-    generate_b = Thread(target=generate_customer, args=(60, "B", customer_b))
-
     generate_a.start()
     time.sleep(1)
     generate_b.start()
     time.sleep(60)
+
     stop.set()
-
-
     baker.stop.set()
     butcher.stop.set()
     cheese.stop.set()
